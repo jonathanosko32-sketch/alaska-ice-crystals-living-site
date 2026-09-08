7e307392aa9bc6dced27d399d9236c56ce0b0688
jonathanosko32-sketch <jonathanosko32@gmail.com>
2026-09-08 15:51:50 -0800

    Add phone launcher app module

---
+++ android-launcher-phonefix/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+    namespace = "com.osko.launcher"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.osko.launcher"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 12
+        versionName = "1.2.0-phonefix"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
