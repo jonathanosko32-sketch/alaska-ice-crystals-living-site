e6c618c94b571688e6afbaa2b54915c3b745702c
jonathanosko32-sketch <jonathanosko32@gmail.com>
2026-09-10 17:12:16 -0800

    Bump coded duplicate Samsung Android test build

--- android-launcher-phonefix/app/build.gradle.kts
+++ android-launcher-phonefix/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.osko.launcher.home.codedduplicate"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1200
-        versionName = "12.0.0-coded-duplicate-master-test"
+        versionCode = 1201
+        versionName = "12.0.1-coded-duplicate-samsung-fix"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
